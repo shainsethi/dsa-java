227f0dccd5ca4071a5051a95833e5af9501f260e
shainsethi <Shainsethi1000@gmail.com>
2025-12-27 22:32:26 +0530

    feat: Add Java examples for conditional statements including number comparisons, even/odd checks, income tax calculation, largest of three, and a basic calculator.

--- ConditionalStatements/ConditionalStatements.java
+++ ConditionalStatements/ConditionalStatements.java
@@ -60,9 +60,9 @@ public class ConditionalStatements {
         int B = 3;
         int C = 6;
 
-        if ((A > B) && (A > C))
+        if ((A >= B) && (A >= C))
             System.out.println("A is largest");
-        else if ((B > C))
+        else if (B >= C)
             System.out.println("B is largest");
         else
             System.out.println("C is largest");
