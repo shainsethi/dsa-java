33d6b2d62fb0842ba473cfec5637bda6ae867ff2
shainsethi <Shainsethi1000@gmail.com>
2026-01-05 21:14:50 +0530

    feat(Arrays): add program to generate and print subarrays

--- Arrays/PartOne/SubArray.java
+++ Arrays/PartOne/SubArray.java
@@ -2,15 +2,19 @@ package Arrays.PartOne;
 
 public class SubArray {
     public static void sub_array(int num[]) {
+        int tsb = 0;
+
         for (int i = 0; i < num.length; i++) {
-            for (int j = i + 1; j < num.length; j++) {
+            for (int j = i; j < num.length; j++) {
                 for (int k = i; k <= j; k++) {
                     System.out.print(num[k]);
                 }
+                tsb++;
                 System.out.println("");
             }
             System.out.println("");
         }
+        System.out.print(tsb);
     }
 
     public static void main(String args[]) {
