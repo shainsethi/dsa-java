1ca9b3141a01dbb1ba312b1b26ecf9c92d78fe79
shainsethi <Shainsethi1000@gmail.com>
2026-01-05 22:33:49 +0530

    feat: added sub array

--- Arrays/PartOne/SubArray.java
+++ Arrays/PartOne/SubArray.java
@@ -3,17 +3,29 @@ package Arrays.PartOne;
 public class SubArray {
     public static void sub_array(int num[]) {
         int tsb = 0;
+        int smallest = Integer.MAX_VALUE;
+        int largest = Integer.MIN_VALUE;
 
         for (int i = 0; i < num.length; i++) {
             for (int j = i; j < num.length; j++) {
+                int sum = 0;
                 for (int k = i; k <= j; k++) {
-                    System.out.print(num[k]);
+                    System.out.print(num[k] + " ");
+                    sum += num[k];
+                }
+                if (smallest > sum) {
+                    smallest = sum;
+                }
+                if (largest < sum) {
+                    largest = sum;
                 }
                 tsb++;
-                System.out.println("");
+                System.out.println(" => Sum = " + sum);
             }
             System.out.println("");
         }
+        System.out.println(" => smallest = " + smallest);
+        System.out.println(" => largest = " + largest);
         System.out.print(tsb);
     }
 
