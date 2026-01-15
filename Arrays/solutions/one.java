f5d5d62c9db9f35b37b0e6894ca80a191b71975f
shainsethi <Shainsethi1000@gmail.com>
2026-01-15 22:11:45 +0530

    feat: Add initial Java solutions for array problems, including a duplicate checker in `one.java` and a new empty `two.java`.

---
+++ Arrays/solutions/one.java
@@ -0,0 +1,20 @@
+package Arrays.solutions;
+
+public class one {
+
+    public static boolean isDuplicate(int n[]) {
+        for (int i = 0; i < n.length; i++) {
+            for (int j = i + 1; j < n.length; j++) {
+                if (n[i] == n[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        int num[] = { 1, 2, 3, 4 };
+        System.out.println(isDuplicate(num));
+    }
+}
