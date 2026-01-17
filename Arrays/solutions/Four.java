4ad30c421eca6c92c1c1d743585c3d84b5d333ad
shainsethi <Shainsethi1000@gmail.com>
2026-01-17 16:40:22 +0530

    feat: Add new array problems, including a solution for maximum profit.

---
+++ Arrays/solutions/Four.java
@@ -0,0 +1,16 @@
+package Arrays.Solutions;
+
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j],  nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.Notice that the solution set must not contain duplicate triplets.
+*/
+
+public class Four {
+
+    public static int[] triplets(int n[]) {
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
