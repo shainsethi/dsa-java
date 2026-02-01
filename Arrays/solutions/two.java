6e68c8d6e9adcd1d549bf3866f6850fa2a6cf8ba
shainsethi <Shainsethi1000@gmail.com>
2026-02-01 22:26:52 +0530

    feat: add implementations for Insertion Sort, Maximum Subarray Sum, and Search in Rotated Sorted Array.

--- Arrays/solutions/two.java
+++ Arrays/solutions/two.java
@@ -10,7 +10,25 @@ Example 2:Input:nums = [4,  5, 6, 7, 0, 1, 2], target = 3Output:   -1
 
 public class Two {
 
-    public static void main(String args[]) {
+    public static int findIndex(int nums[] , int target){
 
+        int min = searchMin(nums);
+
+    }
+
+    public static int searchMin(int nums[]){
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left < right){
+            int midkey = left + (right - left) / 2;
+
+            if(midkey > 0 && arr[midkey - 1])
+        }
+    }
+
+    public static void main(String args[]) {
+        int nums = {7 , 6 , 5 , 4 , 0 , 1  , 2 , 3};
+        findIndex(nums,0);
     }
 }
