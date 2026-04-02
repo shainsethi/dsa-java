8245025d44cb771107f60a0054033f2c4b894210
shainsethi <Shainsethi1000@gmail.com>
2026-04-02 21:59:04 +0530

    feat: add number class to check if an integer is even using bitwise operators

---
+++ Bitwise/number.java
@@ -0,0 +1,11 @@
+package Bitwise;
+
+public class number {
+
+    public static void main(String args[]) {
+
+        int n = 5;
+
+        System.out.print((n & 1) == 0 ? "true" : "false");
+    }
+}
