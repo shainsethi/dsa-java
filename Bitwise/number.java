81b9b65709087b714a82cbebd39b7e3f2db64daf
shainsethi <Shainsethi1000@gmail.com>
2026-04-04 22:00:23 +0530

    feat: add bitwise operation examples for bit manipulation tasks

--- Bitwise/number.java
+++ Bitwise/number.java
@@ -38,5 +38,44 @@ public class number {
 
         System.out.println((n & (~bitMask)));
 
+        // get ith bit
+        int x = 1010;
+        int y = 1;
+
+        int bitmask = 1 << y;
+        System.out.print((x & bitmask) == 0 ? "0" : "1");
+
+        // set ith bit
+        System.out.print(x | bitmask);
+
+        // clear ith bith
+        System.out.print((n & ~(bitmask)));
+
+        // update ith bit
+        // setbit == 1
+        // clearbit == 0
+
+        int value = 1000;
+        int newbit = 1;
+
+        if (newbit == 0) {
+            System.out.print((value & ~(bitMask)));
+        } else {
+            System.out.print(value | bitMask);
+        }
+
+        // clear last ith bit
+        // ~ = 1 = 0
+        // 0 = 1
+        // 1111 = 1100
+        // 1100
+
+        // -1 ~0 = 1111100 1111111
+        // -1 << I
+        // 1 = 0000100
+
+        int lastbit = 2;
+        int mask = ~(0) << lastbit;
+        System.out.println(value & mask);
     }
 }
